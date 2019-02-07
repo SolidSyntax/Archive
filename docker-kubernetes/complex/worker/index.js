4705dba5124feab6af926a3f43130219fc099a19
Hans Maes <hans.maes@aclvb.be>
2019-02-07 11:26:27 +0100

    docker: comments

--- docker-kubernetes/complex/worker/index.js
+++ docker-kubernetes/complex/worker/index.js
@@ -1,6 +1,8 @@
+//Imports
 const keys = require('./keys');
 const redis = require('redis');
 
+//Create a client
 const redisClient = redis.createClient({
     host: keys.redisHost,
     port: keys.redisPort,
@@ -9,15 +11,17 @@ const redisClient = redis.createClient({
 
 const sub = redisClient.duplicate();
 
-
+//calculations
 function fib(index) {
     return index < 2
         ? 1
         : fib(index - 1) + fib(index - 2);
 }
 
+//calculate on messages
 sub.on('message', (channel, message) => {
     redisClient.hset('values', message, fib(parseInt(message)));
 });
 
+//Listen to inserts
 sub.subscribe('insert');
