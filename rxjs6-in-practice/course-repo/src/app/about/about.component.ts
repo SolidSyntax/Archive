439e958bab3b20f287061859ec81b98e0ff3952b
Hans Maes <Hans.Maes@SolidSyntax.net>
2019-10-28 19:42:17 +0100

    RxJs example project

---
+++ rxjs6-in-practice/course-repo/src/app/about/about.component.ts
@@ -0,0 +1,89 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {concat, fromEvent, interval, noop, Observable, of, timer} from 'rxjs';
+import {error} from '@angular/compiler/src/util';
+import {createHttpObservable} from '../common/util';
+import {map} from 'rxjs/operators';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+  intervals() {
+    const interval$ = interval(1000);
+    interval$.subscribe(value => console.log('Interval 1 => ' + value));
+
+    const interval2Subscription = interval$.subscribe(value => console.log('Interval 2 => ' + value));
+    setTimeout(() => interval2Subscription.unsubscribe(), 5000);
+
+    const timer$ = timer(3000, 1000);
+    timer$.subscribe(value => console.log('Timer 1 => ' + value));
+
+
+    const click$ = fromEvent(document, 'click');
+    click$.subscribe(
+      event => console.log('Click => ' + event),
+      err => console.log(err),
+      () => console.log('completed')  // Error and completion are exclusive
+    );
+  }
+
+  customHttp() {
+    const http$ = new Observable(observer => {
+      fetch('/api/courses')
+        .then(response => {
+          return response.json();
+        })
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => {
+          observer.error(err);
+        });
+    });
+
+    http$.subscribe(
+      response => console.log(response),
+      noop, // No opperation () => {}
+      () => console.log('completed')
+    );
+
+  }
+
+  mapOpperator() {
+    createHttpObservable('/api/courses')
+      .pipe(
+        map(res => Object.values(res['payload']))
+      ).subscribe(
+      response => console.log(response),
+      noop, // No opperation () => {}
+      () => console.log('completed')
+    );
+  }
+
+  concatenation() {
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+
+    const result$ = concat(source1$, source2$, source3$);
+    result$.subscribe(console.log);
+
+    const interval$ = interval(1000);
+
+    // source2$ & source3$ are never activated because interval$ never completes
+    const neverCompletes$ = concat(source1$, interval$, source2$, source3$);
+    neverCompletes$.subscribe(console.log);
+  }
+
+}
