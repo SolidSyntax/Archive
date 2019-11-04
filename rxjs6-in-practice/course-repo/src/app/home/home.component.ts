2bc1137fdea0a093e70d4a987a486b94aaad3ba8
Hans Maes <hans.maes@aclvb.be>
2019-11-04 12:53:00 +0100

    RxJs example project

--- rxjs6-in-practice/course-repo/src/app/home/home.component.ts
+++ rxjs6-in-practice/course-repo/src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from '../model/course';
-import {Observable} from 'rxjs';
-import {map, shareReplay, tap} from 'rxjs/operators';
+import {Observable, timer} from 'rxjs';
+import {delayWhen, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
 
@@ -11,6 +11,16 @@ import {createHttpObservable} from '../common/util';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private static ERROR_COURSE: Course = {
+    id: 0,
+    description: 'Something went wrong',
+    iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/rxjs-in-practice-course.png',
+    courseListIcon: 'https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png',
+    longDescription: 'No courses could be loaded from the server.',
+    category: 'BEGINNER',
+    lessonsCount: 10
+  };
+
   beginnerCourses$: Observable<Course[]>;
   advancedCourses$: Observable<Course[]>;
 
@@ -23,8 +33,33 @@ export class HomeComponent implements OnInit {
     const http$ = createHttpObservable('/api/courses');
 
     const courses$: Observable<Course[]> = http$.pipe(
+      // alternative value error handling strategy
+      /**
+       catchError(err => of([HomeComponent.ERROR_COURSE]))
+       **/
+
+      // catch and rethrow strategy
+      /**
+       catchError(err => {
+         console.log('Error occured', err);
+         return throwError(err);
+       }),
+       */
+
+      // retry error handling strategy
+      retryWhen(errors => errors.pipe(
+        delayWhen(() => timer(2000))
+      )),
+
+      // cleanup logic
+      finalize(() => {
+        console.log('Finalize executed...');
+      }),
       tap(() => console.log('HTTP request executed.')),
       map(res => Object.values(res['payload']) as Course[]),
+
+      // replay the result for each subscription
+      // all previous steps are only executed once
       shareReplay()
     );
 
