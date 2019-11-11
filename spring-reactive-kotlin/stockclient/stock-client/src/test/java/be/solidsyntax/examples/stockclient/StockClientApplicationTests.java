3adc2b0fc0f8bb32e61dc729a9cee8c6f630d469
Hans Maes <Hans.Maes@SolidSyntax.net>
2019-11-11 15:53:03 +0100

    Example: Spring reactive and kotlin

---
+++ spring-reactive-kotlin/stockclient/stock-client/src/test/java/be/solidsyntax/examples/stockclient/StockClientApplicationTests.java
@@ -0,0 +1,13 @@
+package be.solidsyntax.examples.stockclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ spring-reactive-kotlin/stockserver/src/test/kotlin/be/solidsyntax/examples/stockserver/StockServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package be.solidsyntax.examples.stockserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StockServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
