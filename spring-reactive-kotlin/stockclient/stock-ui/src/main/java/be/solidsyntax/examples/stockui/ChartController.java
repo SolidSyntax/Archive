63cf0aa622368e2e3c2eb1b1f57c51ec6bfe8844
Hans Maes <hans.maes@aclvb.be>
2019-11-28 14:29:02 +0100

    Example:Spring reactive and kotlin

--- spring-reactive-kotlin/stockclient/stock-ui/src/main/java/be/solidsyntax/examples/stockui/ChartController.java
+++ spring-reactive-kotlin/stockclient/stock-ui/src/main/java/be/solidsyntax/examples/stockui/ChartController.java
@@ -1,7 +1,9 @@
 package be.solidsyntax.examples.stockui;
 
+import be.solidsyntax.examples.stockclient.WebClientStockClient;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,4 +11,11 @@ public class ChartController {
 
     @FXML
     public LineChart<String, Double> chart;
+
+    private  WebClientStockClient webClientStockClient;
+
+    @Autowired
+    public ChartController(WebClientStockClient webClientStockClient) {
+        this.webClientStockClient = webClientStockClient;
+    }
 }
