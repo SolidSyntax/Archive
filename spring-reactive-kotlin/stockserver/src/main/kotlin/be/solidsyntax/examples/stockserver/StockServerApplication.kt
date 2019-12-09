885f559dd80c16757fa8a023dabfe8c6f4dea944
Hans Maes <Hans.Maes@SolidSyntax.net>
2019-12-09 19:03:39 +0100

    Example:Spring reactive and kotlin

--- spring-reactive-kotlin/stockserver/src/main/kotlin/be/solidsyntax/examples/stockserver/StockServerApplication.kt
+++ spring-reactive-kotlin/stockserver/src/main/kotlin/be/solidsyntax/examples/stockserver/StockServerApplication.kt
@@ -3,6 +3,8 @@ package be.solidsyntax.examples.stockserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.http.MediaType
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -19,19 +21,17 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class RestController() {
+class RestController(val priceService: PriceService) {
 
-    @GetMapping(value = ["/stocks/{symbol}"],
-            produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun prices(@PathVariable symbol: String): Flux<StockPrice> {
-        return Flux
-                .interval(Duration.ofSeconds(1))
-                .map { StockPrice(symbol, randomStockPrice(), LocalDateTime.now()) }
-    }
+    @GetMapping(value = ["/stocks/{symbol}"], produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
+    fun prices(@PathVariable symbol: String) = priceService.generatePrices(symbol)
 
-    private fun randomStockPrice(): Double {
-        return ThreadLocalRandom.current().nextDouble(100.0)
-    }
+}
+
+@Controller
+class RSocketController(val priceService: PriceService) {
+    @MessageMapping("stockPrices")
+    fun prices(symbol: String) = priceService.generatePrices(symbol)
 }
 
 data class StockPrice(val symbol: String,
