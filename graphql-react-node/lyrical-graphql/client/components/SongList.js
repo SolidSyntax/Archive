d638237e921d4b359b3648abb72d27c8054c7602
Hans Maes <hans.maes@aclvb.be>
2020-04-14 16:29:21 +0200

    graphql: apollo for react hooks

--- graphql-react-node/lyrical-graphql/client/components/SongList.js
+++ graphql-react-node/lyrical-graphql/client/components/SongList.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import {useQuery} from "@apollo/react-hooks";
+// https://www.apollographql.com/docs/react/data/queries/#executing-a-query
+const GET_SONGS = gql`
+{
+  songs{
+    title
+  }
+}
+`;
+
 
 function SongList() {
+    const { loading, error, data } = useQuery(GET_SONGS);
+
+    if (loading) return 'Loading...';
+    if (error) return `Error! ${error.message}`;
+
     return (
         <div>
-            SongList
+            {data.songs.map(song => (
+                <div key={song.title}>
+                    {song.title}
+                </div>
+            ))}
         </div>
     );
 }
+
 
 export default SongList;
