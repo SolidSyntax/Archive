babdd7bc46a97ca6dc5c9aeafc3a5a608b6c00c0
Hans Maes <hans.maes@aclvb.be>
2020-05-08 09:23:47 +0200

    graphql: requireAuth hook

--- graphql-react-node/auth-graphql/client/components/Dashboard.js
+++ graphql-react-node/auth-graphql/client/components/Dashboard.js
@@ -1,19 +1,14 @@
 import React from "react";
 import GET_CURRENT_USER from "../queries/CurrentUser";
-import {useMutation, useQuery} from "@apollo/react-hooks";
-import Link from "react-router/lib/Link";
-import LOGOUT from "../mutations/Logout";
-import {hashHistory} from "react-router";
+import {useQuery} from "@apollo/react-hooks";
+import {requireAuth} from "../hooks/requireAuth";
 
 function Dashboard() {
+    requireAuth();
     const {loading, error, data} = useQuery(GET_CURRENT_USER);
 
     if (loading) return null;
     if (error) return `Error! ${error}`;
-
-    if (!data.user) {
-        hashHistory.push('/login')
-    }
 
     return (
         <main><h3>Welcome {JSON.stringify(data)}</h3></main>
---
+++ graphql-react-node/auth-graphql/client/hooks/requireAuth.js
@@ -0,0 +1,15 @@
+import {useQuery} from "@apollo/react-hooks";
+import GET_CURRENT_USER from "../queries/CurrentUser";
+import {useEffect, useRef} from "react";
+import {hashHistory} from "react-router";
+
+export function requireAuth(props) {
+    const {loading, error, data} = useQuery(GET_CURRENT_USER);
+
+    useEffect(() => {
+        if (!loading && !data.user) {
+            hashHistory.push('/login')
+        }
+
+    });
+}
